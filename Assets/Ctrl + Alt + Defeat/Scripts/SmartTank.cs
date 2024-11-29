55a1473c5844427d5ce1ac95daf9b905a4fbe77e
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-11-29 03:11:56 +0000

    Initial Setup

---
+++ Assets/Ctrl + Alt + Defeat/Scripts/SmartTank.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace CAD
+{
+    public class SmartTank : AITank
+    {
+        public override void AITankStart()
+        {
+            // TODO: Implement Start Logic
+        }
+
+        public override void AITankUpdate()
+        {
+            // TODO: Implement Update Logic
+        }
+
+        public override void AIOnCollisionEnter(Collision collision)
+        {
+            // TODO: Implement Collision Response
+        }
+    }
+}
