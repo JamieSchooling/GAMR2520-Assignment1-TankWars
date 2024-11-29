28e980a0779e4e927268d6d45f3c8c66f53f887f
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-11-29 15:10:10 +0000

    feat: Create classes for each state, implementing IState

---
+++ Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/RetreatState.cs
@@ -0,0 +1,20 @@
+namespace CAD
+{
+    public class RetreatState : IState
+    {
+        public void OnStateEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateExit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/SearchState.cs
@@ -0,0 +1,20 @@
+namespace CAD
+{
+    public class SearchState : IState
+    {
+        public void OnStateEnter()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateExit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnStateUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
