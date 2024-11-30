12ab99c154dac062ad96c7bb4129c15f1ee5c1e3
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-11-30 01:27:22 +0000

    feat: Implement simple retreat state

--- Assets/Ctrl + Alt + Defeat/Scripts/SmartTank.cs
+++ Assets/Ctrl + Alt + Defeat/Scripts/SmartTank.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -12,31 +13,48 @@ namespace CAD
         private AttackState m_AttackState = new();
         private RetreatState m_RetreatState = new();
 
+        public Dictionary<GameObject, float> VisibleConsumables => a_ConsumablesFound;
+
+        public void GenerateNewRandomWorldPoint()
+        {
+            a_GenerateRandomPoint();
+        }
+
+        public void FollowPathToWorldPoint(GameObject pointInWorld, float normalizedSpeed)
+        {
+            a_FollowPathToPoint(pointInWorld, normalizedSpeed);
+        }
+
+        public void FollowPathToRandomWorldPoint(float normalizedSpeed)
+        {
+            a_FollowPathToRandomPoint(normalizedSpeed);
+        }
+
         public override void AITankStart()
         {
-            m_CurrentState = m_SearchState;
+            m_CurrentState = m_RetreatState;
         }
 
         public override void AITankUpdate()
         {
             m_CurrentState.OnStateUpdate(this);
 
-            if (a_TanksFound.Count == 0 || !a_TanksFound.First().Key)
-            {
-                SwitchState(m_SearchState);
-            }
-            if (a_TanksFound.Count > 0 && a_TanksFound.First().Key)
-            {
-                SwitchState(m_ChaseState);
-            }
-            if (m_CurrentState == m_ChaseState && Vector3.Distance(transform.position, a_TanksFound.First().Key.transform.position) < 25.0f)
-            {
-                SwitchState(m_AttackState);
-            }
-            if (a_GetHealthLevel < 30.0f || a_GetAmmoLevel < 4.0f || a_GetFuelLevel < 50.0f)
-            {
-                SwitchState(m_RetreatState);
-            }
+            //if (a_TanksFound.Count == 0 || !a_TanksFound.First().Key)
+            //{
+            //    SwitchState(m_SearchState);
+            //}
+            //if (a_TanksFound.Count > 0 && a_TanksFound.First().Key)
+            //{
+            //    SwitchState(m_ChaseState);
+            //}
+            //if (m_CurrentState == m_ChaseState && Vector3.Distance(transform.position, a_TanksFound.First().Key.transform.position) < 25.0f)
+            //{
+            //    SwitchState(m_AttackState);
+            //}
+            //if (a_GetHealthLevel < 30.0f || a_GetAmmoLevel < 4.0f || a_GetFuelLevel < 50.0f)
+            //{
+            //    SwitchState(m_RetreatState);
+            //}
         }
 
         public override void AIOnCollisionEnter(Collision collision)
--- Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/RetreatState.cs
+++ Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/RetreatState.cs
@@ -1,15 +1,34 @@
+using UnityEngine;
+using System.Linq;
+
 namespace CAD
 {
     public class RetreatState : IState
     {
+        private float m_CurrentTime;
+
         public void OnStateEnter(SmartTank tankAI)
         {
-            throw new System.NotImplementedException();
+            m_CurrentTime = 0.0f;
         }
 
         public void OnStateUpdate(SmartTank tankAI)
         {
-            throw new System.NotImplementedException();
+            if (tankAI.VisibleConsumables.Count > 0)
+            {
+                GameObject consumable = tankAI.VisibleConsumables.First().Key;
+                tankAI.FollowPathToWorldPoint(consumable, 1f);
+                m_CurrentTime += Time.deltaTime;
+                if (m_CurrentTime > 10)
+                {
+                    tankAI.GenerateNewRandomWorldPoint();
+                    m_CurrentTime = 0;
+                }
+            }
+            else
+            {
+                tankAI.FollowPathToRandomWorldPoint(1f);
+            }
         }
 
         public void OnStateExit(SmartTank tankAI)
