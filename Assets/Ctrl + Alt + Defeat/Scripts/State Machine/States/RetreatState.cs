12ae885d2ad76a312164dc265709c7453cb0a1d6
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-11-30 02:28:32 +0000

    feat: Update retreat state to search for specific consumable needed

--- Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/RetreatState.cs
+++ Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/RetreatState.cs
@@ -5,35 +5,62 @@ namespace CAD
 {
     public class RetreatState : IState
     {
-        private float m_CurrentTime;
+        private float m_CurrentTime = 0;
 
         public void OnStateEnter(SmartTank tankAI)
         {
-            m_CurrentTime = 0.0f;
+            Debug.Log("Enter Retreat State");
         }
 
         public void OnStateUpdate(SmartTank tankAI)
         {
+            if (tankAI.Health <= 30.0f)
+            {
+                FindConsumable(tankAI, "Health");
+            }
+            if (tankAI.Ammo <= 4.0f)
+            {
+                FindConsumable(tankAI, "Ammo");
+            }
+            if (tankAI.Fuel <= 50.0f)
+            {
+                FindConsumable(tankAI, "Fuel");
+            }
+        }
+
+        public void OnStateExit(SmartTank tankAI)
+        {
+            // TODO: Implement OnStateExit
+        }
+
+        private void FindConsumable(SmartTank tankAI, string consumableType)
+        {
             if (tankAI.VisibleConsumables.Count > 0)
             {
-                GameObject consumable = tankAI.VisibleConsumables.First().Key;
-                tankAI.FollowPathToWorldPoint(consumable, 1f);
-                m_CurrentTime += Time.deltaTime;
-                if (m_CurrentTime > 10)
+                var potentialConsumables = tankAI.VisibleConsumables.Where(c => c.Key.CompareTag(consumableType)).ToDictionary(i => i.Key, i => i.Value);
+
+                if (potentialConsumables.Count > 0)
                 {
-                    tankAI.GenerateNewRandomWorldPoint();
-                    m_CurrentTime = 0;
+                    GameObject consumable = potentialConsumables.First().Key;
+                    tankAI.FollowPathToWorldPoint(consumable, 1f);
+                    m_CurrentTime += Time.deltaTime;
+                }
+                else
+                {
+                    tankAI.FollowPathToRandomWorldPoint(1f);
                 }
             }
             else
             {
                 tankAI.FollowPathToRandomWorldPoint(1f);
             }
-        }
 
-        public void OnStateExit(SmartTank tankAI)
-        {
-            throw new System.NotImplementedException();
+            m_CurrentTime += Time.deltaTime;
+            if (m_CurrentTime > 10)
+            {
+                tankAI.GenerateNewRandomWorldPoint();
+                m_CurrentTime = 0;
+            }
         }
     }
 }
