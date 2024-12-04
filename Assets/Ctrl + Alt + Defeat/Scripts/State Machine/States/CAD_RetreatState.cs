5b9187d06755214df58a060c133ebfe52fee1ac2
Kye Hollowood <P2726430@LEC-ADMIN.dmu.ac.uk>
2024-12-04 17:05:41 +0000

    feature: retreat around enemy and bases

--- Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/CAD_RetreatState.cs
+++ Assets/Ctrl + Alt + Defeat/Scripts/State Machine/States/CAD_RetreatState.cs
@@ -12,8 +12,27 @@ public class CAD_RetreatState : CAD_State
     /// Stores the last known position of the enemy tank.
     /// </summary>
     private Vector3 m_EnemyPos;
-    private GameObject m_NewPos;
-    Vector3 Offset = new Vector3(25, 0, 25);
+    /// <summary>
+    /// Holds a Positive x offset
+    /// </summary>
+    Vector3 XPOffset = new Vector3(25, 0, 0);
+    /// <summary>
+    /// Holds a Negative x offset
+    /// </summary>
+    Vector3 XNOffset = new Vector3(-25, 0, 0);
+    /// <summary>
+    /// Holds a Positive z offset
+    /// </summary>
+    Vector3 ZPOffset = new Vector3(0, 0, 25);
+    /// <summary>
+    /// Holds a Negative z offset
+    /// </summary>
+    Vector3 ZNOffset = new Vector3(0, 0, -25);
+
+    /// <summary>
+    /// Holds the current waypint game object
+    /// </summary>
+    private GameObject m_CurrentWaypoint;
 
     public override void OnStateEnter(CAD_SmartTank tankAI)
     {
@@ -29,37 +48,69 @@ public class CAD_RetreatState : CAD_State
         GameObject SafestPos = new GameObject("SafestPos");
         SafestPos.transform.position = m_EnemyPos * -1;
         tankAI.LastKnownSafestPos = SafestPos;
-        if (tankAI.EnemyTank)
-        {
-            Vector3 TankOffset = tankAI.EnemyTank.transform.position + Offset;
-            m_NewPos = new GameObject("MoveAround");
-            m_NewPos.transform.position = TankOffset;
-            tankAI.FollowPathToWorldPoint(m_NewPos, 1f);
-            Destroy(m_NewPos);
-        }
         if (tankAI.VisibleEnemyBases.Count > 0)
         {
-            if (Vector3.Distance(tankAI.transform.position, tankAI.VisibleEnemyBases.First().Key.transform.position) < 25.0f)
+            if (Vector3.Distance(tankAI.transform.position, tankAI.VisibleEnemyBases.First().Key.transform.position) < 50.0f)
             {
-                Vector3 EBaseOffset = tankAI.VisibleEnemyBases.First().Key.transform.position + Offset;
-                m_NewPos = new GameObject("MoveAround");
-                m_NewPos.transform.position = EBaseOffset;
-                tankAI.FollowPathToWorldPoint(m_NewPos, 1f);
-                Destroy(m_NewPos);
+                Vector3 EBaseOffset = tankAI.VisibleEnemyBases.First().Key.transform.position + XPOffset + ZPOffset;
+                m_CurrentWaypoint = tankAI.CreateWaypoint(EBaseOffset);
+                tankAI.FollowPathToWorldPoint(m_CurrentWaypoint, 1f);
+                Destroy(m_CurrentWaypoint);
             }
         }
         if (tankAI.FriendlyBases.Count > 0)
         {
             var closestfbase = tankAI.FriendlyBases.OrderBy(b => b.Value).First().Key;
-            if (Vector3.Distance(tankAI.transform.position, closestfbase.transform.position) < 25.0f)
+            if (Vector3.Distance(tankAI.transform.position, closestfbase.transform.position) < 50.0f)
             {
-                Vector3 FBaseOffset = closestfbase.transform.position + Offset;
-                m_NewPos = new GameObject("MoveAround");
-                m_NewPos.transform.position = FBaseOffset;
-                tankAI.FollowPathToWorldPoint(m_NewPos, 1f);
-                Destroy(m_NewPos);
+                Vector3 FBaseOffset = closestfbase.transform.position + XNOffset + ZNOffset;
+                m_CurrentWaypoint = tankAI.CreateWaypoint(FBaseOffset);
+                tankAI.FollowPathToWorldPoint(m_CurrentWaypoint, 1f);
+                Destroy(m_CurrentWaypoint);
             }
         }
+        if (tankAI.EnemyTank)
+        {
+            if (Vector3.Distance(tankAI.transform.position, tankAI.EnemyTank.transform.position) >= 50.0f)
+            {
+                PathtoSafe(tankAI);
+                return;
+            }
+
+            if (tankAI.transform.position.x > tankAI.EnemyTank.transform.position.x)
+            {
+                Vector3 TankOffset = tankAI.EnemyTank.transform.position + XPOffset;
+                m_CurrentWaypoint = tankAI.CreateWaypoint(TankOffset);
+                tankAI.FollowPathToWorldPoint(m_CurrentWaypoint, 1f);
+                Destroy(m_CurrentWaypoint);
+            }
+            if (tankAI.transform.position.x < tankAI.EnemyTank.transform.position.x)
+            {
+                Vector3 TankOffset = tankAI.EnemyTank.transform.position + XNOffset;
+                m_CurrentWaypoint = tankAI.CreateWaypoint(TankOffset);
+                tankAI.FollowPathToWorldPoint(m_CurrentWaypoint, 1f);
+                Destroy(m_CurrentWaypoint);
+            }
+            if (tankAI.transform.position.z > tankAI.EnemyTank.transform.position.z)
+            {
+                Vector3 TankOffset = tankAI.EnemyTank.transform.position + ZPOffset;
+                m_CurrentWaypoint = tankAI.CreateWaypoint(TankOffset);
+                tankAI.FollowPathToWorldPoint(m_CurrentWaypoint, 1f);
+                Destroy(m_CurrentWaypoint);
+            }
+            if (tankAI.transform.position.z < tankAI.EnemyTank.transform.position.z)
+            {
+                Vector3 TankOffset = tankAI.EnemyTank.transform.position + ZNOffset;
+                m_CurrentWaypoint = tankAI.CreateWaypoint(TankOffset);
+                tankAI.FollowPathToWorldPoint(m_CurrentWaypoint, 1f);
+                Destroy(m_CurrentWaypoint);
+            }
+        }
+        PathtoSafe(tankAI);
+    }
+
+    private void PathtoSafe(CAD_SmartTank tankAI)
+    {
         tankAI.FollowPathToWorldPoint(tankAI.LastKnownSafestPos, 1f);
         if (tankAI.EnemyTank) m_EnemyPos = tankAI.EnemyTank.transform.position;
     }
