2d17aecefc4907243c58e5d0b2cf5291d0b29161
JamieSchooling <99397650+JamieSchooling@users.noreply.github.com>
2024-12-15 23:41:33 +0000

    docs: Update editor script docs

--- Assets/Ctrl + Alt + Defeat/Scripts/RBS/Rule System/RBSEditor/Editor/CAD_RulesAssetOpener.cs
+++ Assets/Ctrl + Alt + Defeat/Scripts/RBS/Rule System/RBSEditor/Editor/CAD_RulesAssetOpener.cs
@@ -1,37 +1,42 @@
 using UnityEditor;
 using UnityEngine;
 
-public class CAD_DecisionRulesAssetOpener : AssetModificationProcessor
+/// <summary>
+/// Automatically opens a custom editor window for rule editing when a CAD_Rules asset is double-clicked.
+/// </summary>
+public class CAD_RulesAssetOpener : AssetModificationProcessor
 {
-    // Called when an asset is double-clicked
     /// <summary>
-    /// creates an editor asset that can be used to make a set of rules
+    /// Callback triggered when an asset is opened in the Unity editor.
+    /// If the opened asset is of type <see cref="CAD_Rules"/>, it opens a custom rules editor window.
     /// </summary>
-    /// <param name="instanceID"></param>
-    /// <param name="line"></param>
-    /// <returns></returns>
+    /// <param name="instanceID">The instance ID of the opened asset.</param>
+    /// <param name="line">The line number clicked in the asset (unused).</param>
+    /// <returns>
+    /// Returns true if the asset was handled (opened in the custom editor), otherwise false to allow default Unity behavior.
+    /// </returns>
     [UnityEditor.Callbacks.OnOpenAsset]
     public static bool OnOpenAsset(int instanceID, int line)
     {
-        // Get the selected asset   
+        // Get the selected asset by its instance ID.
         Object obj = EditorUtility.InstanceIDToObject(instanceID);
 
-        // Check if the selected asset is of type CAD_Rules
+        // Check if the selected asset is of type CAD_Rules.
         if (obj is CAD_Rules decisionRules)
         {
-            // Open the custom editor window
+            // Open the custom rules editor window.
             CAD_RulesEditorWindow window = EditorWindow.GetWindow<CAD_RulesEditorWindow>();
             window.titleContent = new GUIContent("Rules Editor");
             window.Show();
 
-            // Assign the asset to the editor window
+            // Assign the opened CAD_Rules asset to the editor window.
             window.SetAsset(decisionRules);
 
-            // Return true to indicate that the asset was handled
+            // Indicate that the asset was handled by this custom logic.
             return true;
         }
 
-        // Return false to allow Unity's default behavior for other assets
+        // Return false to allow Unity's default behavior for non-CAD_Rules assets.
         return false;
     }
 }
